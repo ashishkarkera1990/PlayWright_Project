c09efeb3160db906f5d3adb0a6fbce9206e265df
ashishkarkera1990 <105056288+ashishkarkera1990@users.noreply.github.com>
2024-07-12 15:43:11 +0530

    Updated Reporter

--- playwright.config.js
+++ playwright.config.js
@@ -20,9 +20,9 @@ module.exports = defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  //reporter: [["line"], ["allure-playwright"]],
+  /* Reporter to use. See https://playwright.dev/docs/tesclet-reporters */
+  //reporter: 'html',
+  reporter: [["line"], ["allure-playwright"]],
   
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
